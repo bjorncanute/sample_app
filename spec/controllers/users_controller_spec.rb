586095eaf7677368c0ccfe87e5d2e740f05768d4
Bjorn <Bjorn@Bjorns-Mac-mini.local>
2014-02-24 21:26:17 -0600

    Finished User signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -54,4 +54,58 @@ describe UsersController do
     end
   end
 
+  describe "POST 'create'" do
+
+    describe "Failiure" do
+
+      before(:each) do
+        @attr = { :name => "", 
+                  :email => "", 
+                  :password => "", 
+                  :password_confirmation => ""
+                }
+      end
+
+      it "should show 'signup title'" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end
+
+      it "it should render new page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+    end
+
+    describe "Success" do
+      before(:each) do
+        @attr = { :name => "New User", 
+                  :email => "newuser@example.com", 
+                  :password => "foobar", 
+                  :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app!/i
+      end
+    end
+  end
 end
