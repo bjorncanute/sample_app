44c3d579f02075d0e158c13c62215faf6f9ed036
Bjorn <Bjorn@Bjorns-Mac-mini.local>
2014-02-24 22:53:38 -0600

    Handling invalid login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,8 +13,29 @@ describe SessionsController do
     	get :new
     	response.should have_selector('title', :content => "Sign in")
     end
-
-
   end
+  describe "POST 'create'" do
+    describe "Failure" do
+      before(:each) do
+        # @attr = { :email => "name@example.com", :password => "foobar" }
+        @attr = { :email => "", :password => "" }
+      end
 
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+
+      it "should still have the right title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => "Sign in")
+      end
+    end
+  
+  end
 end
