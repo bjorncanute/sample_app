86fe30d253926099ab67084182125257adb1f851
Bjorn <Bjorn@Bjorns-Mac-mini.local>
2014-02-27 21:17:47 -0600

    Can now destory users (as admins)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean          default(FALSE)
 #
 
 require 'spec_helper'
@@ -144,11 +145,9 @@ describe User do
 			end
 		end
 
-		describe "User authentication" do
+		describe "User authentication method" do
 
-			# before(:each) do
-			# 	@user = User.new(@attr)
-			# end
+			
 
 			it "should have authenticate method" do
 				# @user.should respond_to(:authenticate)
@@ -169,12 +168,25 @@ describe User do
 		end
 	end
 
+	describe "admin attribute" do
 
+		before(:each) do
+			@user = User.create!(@attr)
+		end
 
+		it "should respond to admin" do
+			@user.should respond_to(:admin)
+		end
 
+		it "should not be an admin by default" do
+			@user.should_not be_admin
+		end
 
-
-	
+		it "should be convertible to an admin" do
+			@user.toggle!(:admin)
+			@user.should be_admin
+		end
+	end
 end
 
 
