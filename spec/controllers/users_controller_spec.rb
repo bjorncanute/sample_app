ab397efcdfaedf43bd0932d147d450766edfc16c
Bjorn <Bjorn@Bjorns-Mac-mini.local>
2014-03-02 02:41:49 -0600

    Showing microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -105,7 +105,28 @@ describe UsersController do
     it "should have the right URL" do
       get :show, :id => @user
       response.should have_selector('div>p>a', :content => user_path(@user),
-                                                          :href    => user_path(@user))
+                                               :href    => user_path(@user))
+    end
+
+    it "should show the users microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector('.content', :content => mp1.content)
+      response.should have_selector('.content', :content => mp2.content)
+    end
+
+    it "should paginate microposts" do
+      35.times { Factory(:micropost, :user => @user, :content => "foo bar") }
+      get :show, :id => @user
+      response.should have_selector('div.pagination')
+    end
+
+    it "should display the micropost count" do
+      # 10.times { Factory(:micropost, :user => @user, :content => "foo bar") }
+      get :show, :id => @user
+      response.should have_selector('.count', 
+                                     :content => @user.microposts.count.to_s )
     end
   end
 
